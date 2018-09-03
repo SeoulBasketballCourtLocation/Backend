e57dce5a8fdceacf673dc460113a8910a95a8d5a
chrisaor <pchpch0070@gmail.com>
2018-09-03 14:07:53 +0900

    courts model 작성

--- app/courts/models.py
+++ app/courts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Court(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    coordinate_x = models.CharField(max_length=255)
+    coordinate_y = models.CharField(max_length=255)
+    no_basket = models.IntegerField()
+    bench = models.BooleanField(default=False)
+    showerbox = models.BooleanField(default=False)
+    parking = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
