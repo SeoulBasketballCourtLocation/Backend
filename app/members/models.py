154eea2bc87db77114a64e7b5a3e9b10e1d4afb2
chrisaor <pchpch0070@gmail.com>
2018-09-03 14:08:57 +0900

    members앱 생성 및 model작성

---
+++ app/members/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    CHOICE_GENDER = (
+        ('m', '남성'),
+        ('f', '여성'),
+        ('x', '선택안함')
+    )
+    img_profile = models.ImageField(upload_to='user', blank=True)
+    introduce = models.TextField(blank=True)
+    gender = models.CharField(max_length=1, choices=CHOICE_GENDER)
+
+    def __str__(self):
+        return self.username
