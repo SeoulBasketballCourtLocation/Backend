22bfa08b15026e55b2040e9c67bcc2bb1c11c407
chrisaor <pchpch0070@gmail.com>
2018-09-05 18:08:45 +0900

    signup form 및 template 작성

---
+++ app/members/forms.py
@@ -0,0 +1,9 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
+
+User = get_user_model()
+
+class SignupForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('img_profile', 'username', 'password1', 'password2', 'email', 'phone_number', 'gender', 'main_position', 'second_position')
--- app/members/views.py
+++ app/members/views.py
@@ -1,5 +1,18 @@
+from django.contrib.auth import login
 from django.shortcuts import render
 
+from members.forms import SignupForm
+
+
 def signup(request):
-    # if request.method == 'POST':
-    return render(request, 'members/signup.html')
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return render(request, 'members/signup-done.html')
+    else:
+        form = SignupForm()
+    context = {
+        'form':form,
+    }
+    return render(request, 'members/signup.html', context)
