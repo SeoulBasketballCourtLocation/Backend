c932739d53ad61d09017ba9bfa5ee955482dacd5
chrisaor <pchpch0070@gmail.com>
2018-09-08 15:20:05 +1000

    added introduce field in signup form

--- app/members/forms.py
+++ app/members/forms.py
@@ -7,4 +7,4 @@ User = get_user_model()
 class SignupForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('img_profile', 'username', 'password1', 'password2', 'email', 'phone_number', 'gender', 'main_position', 'second_position')
+        fields = ('img_profile', 'username', 'password1', 'password2', 'email', 'phone_number', 'gender', 'main_position', 'second_position', 'introduce')
