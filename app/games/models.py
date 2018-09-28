951a029d7207dba558e58cbc18833778094189ed
chrisaor <pchpch0070@gmail.com>
2018-09-28 14:28:02 +0900

    added Game model

--- app/games/models.py
+++ app/games/models.py
@@ -1,3 +1,20 @@
+from datetime import timezone
+
+from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+from courts.models import Court
+
+User = get_user_model()
+
+class Game(models.Model):
+    host = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    court = models.ForeignKey(Court, on_delete=models.CASCADE)
+    curr_player = models.IntegerField(default=1)
+    need_player = models.IntegerField(default=1)
+    created_time = models.DateTimeField(auto_now_add=True)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+
